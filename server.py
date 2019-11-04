b9d999af512cf92839e744c3a777d6a2e11af7b8
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-04 14:35:52 -0800

    completed all ninja and sensei bonuses

--- server.py
+++ server.py
@@ -3,24 +3,38 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 
-
 @app.route('/')
 def index():
-    if 'count' in session:
-        session['count'] += 1
-    else:
+    if 'count' not in session:
         session['count'] = 1
-    return render_template('index.html', count=session['count'])
+
+    if 'visits' not in session:
+        session['visits'] = 1
+    else:
+        session['visits'] += 1
+    return render_template('index.html', count=session['count'], visits=session['visits'])
+
 
 @app.route('/increment', methods=['POST'])
 def increment():
-  session['count'] += 1
-  return redirect('/')
+    if 'custom_increment' in request.form:
+        session['custom_increment'] = request.form["custom_increment"]
+        session['count'] += int(session['custom_increment'])
+        session['visits'] -= 1
+    elif 'increment_by_two' in request.form:
+        session['count'] += 2
+        session['visits'] -= 1
+    elif 'increment_by_one' in request.form:
+        session['count'] += 1
+        session['visits'] -= 1
+    return redirect('/')
+
 
 @app.route('/destroy_session', methods=['POST'])
 def destroy():
-  session.clear()
-  return redirect('/')
+    session.clear()
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
